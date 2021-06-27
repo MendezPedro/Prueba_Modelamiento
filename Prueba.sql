8f6e77cde3bb45534d468771e244654b56b04ecf
Pedro <mendez.pedro94@gmail.com>
2021-06-27 15:08:57 -0400

    consultas

--- Prueba.sql
+++ Prueba.sql
@@ -60,3 +60,48 @@ select * from books;
 select * from authors;
 select * from author_books;
 select * from loans;
+
+--3 consultas
+--A
+select title, number_pages from books where number_pages < 300;
+--B
+select name_author, last_name, birth from authors where birth > 1970;
+--C
+select title 
+from books B
+inner join loans L 
+on B.isbn = L.book_isbn;
+
+select book_isbn, count(book_isbn) AS total
+from loans L
+group by book_isbn
+order by total desc
+limit 1;
+
+-- intentos
+select title 
+from (select book_isbn, count(book_isbn) AS total
+from loans P
+group by book_isbn
+order by total desc) as x
+inner join loans L 
+on x.isbn = L.book_isbn;
+
+--dsghgrgf
+select book_isbn, count(book_isbn) AS total
+from (select title 
+from books B
+inner join loans L 
+on B.isbn = L.book_isbn) as x
+group by book_isbn
+order by total desc
+limit 1;
+
+
+--D
+select L.*, B.loan_days, B.title
+from loans as L 
+inner join Books as B 
+on L.book_isbn = B.isbn;
+--where extract(day from B.fecha) = 06;
+
