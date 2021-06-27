6bc5e8b2d7d36eba5a6e8db0d19e5085910a08c6
Pedro <mendez.pedro94@gmail.com>
2021-06-27 00:16:55 -0400

    80%

--- Prueba.sql
+++ Prueba.sql
@@ -5,37 +5,58 @@ CREATE DATABASE biblioteca;
 
 \c biblioteca
 
-create table socios (
-    rut varchar(15),
-    nombre varchar(10),
-    apellido varchar(10),
-    direccion varchar(50),
-    telefono int
+create table partners (
+    id serial primary key,
+    dni varchar(15),
+    name_partner varchar(10),
+    last_name varchar(10),
+    address_partner varchar(50),
+    phone int
 );
 
-create table libros (
-    isbn varchar(15),
-    titulo varchar(30),
-    pagina int,
-    cod_autor int,
-    nombre_autor varchar(10),
-    apellido_autor varchar(10),
-    nacimiento_muerte  varchar(10),
-    tipo_autor varchar(10),
-    dias_prestamo int
+create table authors (
+    id serial primary key,
+    name_author varchar(10),
+    last_name varchar(10),
+    birth int,
+    die int,
+    type_author varchar(10)
 );
 
-create table prestamos (
-    socio varchar(25),
-    libro varchar(25),
-    fecha_prestamo varchar(10),
-    fecha_devolucion varchar(10)
+create table books (
+    isbn varchar(15) primary key,
+    title varchar(30),
+    number_pages int,
+    author_id int,
+    co_author_id int,
+    loan_days int,
+    FOREIGN KEY (author_id) REFERENCES authors(id)
 );
 
-\copy socios from 'socios.csv' csv header;
-\copy libros from 'libros.csv' csv header;
-\copy prestamos from 'historial_de_prestamos.csv' csv header;
+create table author_books (
+    author_id int,
+    book_isbn varchar(15),
+    FOREIGN KEY (author_id) REFERENCES authors(id),
+    FOREIGN KEY (book_isbn) REFERENCES books(isbn)
+);
 
-select * from socios;
-select * from libros;
-select * from prestamos;
+create table loans (
+    begin_date date,
+    return_true date,
+    partner_id int,
+    book_isbn varchar(15),
+    FOREIGN KEY (partner_id) REFERENCES partners(id),
+    FOREIGN KEY (book_isbn) REFERENCES books(isbn)
+);
+
+\copy partners from 'socios.csv' csv header;
+\copy authors from 'autores.csv' csv header;
+\copy books from 'libros.csv' csv header;
+\copy author_books from 'autor_libro.csv' csv header;
+\copy loans from 'historial_de_prestamos.csv' csv header;
+
+select * from partners;
+select * from books;
+select * from authors;
+select * from author_books;
+select * from loans;
